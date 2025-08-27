19629e7286faacd9bbe04be9b46bbabf77e43cb8
Matheus Mariani <mhgonmariani@gmail.com>
2025-08-27 16:13:51 -0300

    Add files via upload

---
+++ Códiogo J.S.js
@@ -0,0 +1,8 @@
+// lista-01-udwmj-2025/script.js
+
+const tarefas = ["Estudar JS", "Praticar PHP", "Subir projeto no GitHub"];
+
+console.log("Minhas Tarefas:");
+tarefas.forEach((tarefa, i) => {
+  console.log(`${i + 1} - ${tarefa}`);
+});
